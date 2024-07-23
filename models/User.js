13285a3c331ac6ee5ee485e1a6ff946960ded533
rtamirasa <rtamirasa@gmail.com>
2024-07-23 16:51:22 -0400

    authentication

---
+++ models/User.js
@@ -0,0 +1,49 @@
+/*first user*/
+import mongoose from "mongoose";
+const UserSchema = new mongoose.Schema(
+{
+    firstName: {
+        type:String,
+        required: true,
+        min:2,
+        max: 50,
+
+    },
+    lastName: {
+        type:String,
+        required: true,
+        min:2,
+        max: 50,
+
+    },
+    email: {
+        type:String,
+        required: true,
+        max: 50,
+        unique: true,
+
+    },
+    password: {
+        type:String,
+        required: true,
+        min: 5,
+        max: 50,
+        unique: true,
+
+    },
+    picturePath: {
+        type:String,
+        default: "",
+
+    },
+    friends: {
+        type:Array,
+        default: []
+    },
+    location: String,
+    occupation: String,
+    viewedProfile: Number,
+    impressions: Number,
+
+}
+);
