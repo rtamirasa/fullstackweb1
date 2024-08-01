06bfb07a937ad79f3f8db9340a0acff445d88036
rtamirasa <rtamirasa@gmail.com>
2024-08-01 15:53:56 -0400

    navbar

--- fullproject/client/src/App.js
+++ fullproject/client/src/App.js
@@ -3,19 +3,27 @@ import {BrowserRouter, Navigate, Routes, Route} from 'react-router-dom';
 import HomePage from 'scenes/homePage';
 import loginPage from 'scenes/loginPage';
 import profilePage from 'scenes/profilePage';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { createTheme } from '@mui/material';
+import { themeSettings } from 'theme';
+import { Css } from '@mui/icons-material';
 
 
 function App() {
-
+  const mode = useSelector((state) => state.mode);
+  const theme = useMemo(() =>createTheme(themeSettingsmode), [mode]);
   return (
     <div className="app">
+
       <BrowserRouter>
+      <ThemeProvider theme = {theme}>
+        <CssBaseline/>
       <Routes>
         <Route path = "/" element = {<LoginPage/>} />
       <Route path = "/home" element = {<HomePage/>} />
       <Route path = "/profile/:userId" element = {<ProfilePage/>} />
       </Routes>
-      
+      </ThemeProvider>
       </BrowserRouter>
       
     </div>
