5f4602aa8ccb3c8a7514090b8d400cfb3a8d9fb7
rtamirasa <rtamirasa@gmail.com>
2024-08-01 16:11:22 -0400

    more nabar and login

--- fullproject/client/src/scenes/loginPage/index.jsx
+++ fullproject/client/src/scenes/loginPage/index.jsx
@@ -1,7 +1,39 @@
+import {Box, Typography, useTheme, useMediaQuery} from "@mui/material";
+import Form from "./Form";
+
+
 const loginPage = () => {
+    const theme = useTheme();
+    const isNonMobileScreens = useMediaQuery("(min-width: 100px)");
 
-    return <div> homepage</div>;
-
-        
+    return <Box>
+        <Box width = "100%" 
+        backgroundColor  = {theme.palette.background.alt}
+        p='1rem 6%'
+        textAlign = "center">
+    <Typography
+          fontWeight="bold"
+          fontSize="32px"
+          color="primary"
+          onClick={() => navigate("/home")}
+         
+        >
+          Sociopedia
+        </Typography>
+        </Box>
+{/*form boxes here*/} 
+        <Box
+        width = {isNonMobileScreens ? "50%" : "93%"}
+        p = "2rem"
+        m = "2rem auto"
+        borderRadius = "1.5rem"
+        backgroundColor = {theme.pallete.background.alt}
+       >
+        <Typography fontWeight = "500" variant = "h5" sx = {{mb: "1.5rem"}}>
+            Welcome to Scoiapedia!
+        </Typography>
+        <Form />
+         </Box>
+    </Box>
     };
     export default loginPage;
