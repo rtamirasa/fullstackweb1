0130b567fd72cc5b78adda17abebb35ecdccad2d
rtamirasa <rtamirasa@gmail.com>
2024-08-01 19:31:15 -0400

    login fINALLY WOKRING

--- fullproject/client/src/scenes/loginPage/index.jsx
+++ fullproject/client/src/scenes/loginPage/index.jsx
@@ -1,39 +1,36 @@
-import {Box, Typography, useTheme, useMediaQuery} from "@mui/material";
+import { Box, Typography, useTheme, useMediaQuery } from "@mui/material";
 import Form from "./Form";
 
-
-const loginPage = () => {
-    const theme = useTheme();
-    const isNonMobileScreens = useMediaQuery("(min-width: 100px)");
-
-    return <Box>
-        <Box width = "100%" 
-        backgroundColor  = {theme.palette.background.alt}
-        p='1rem 6%'
-        textAlign = "center">
-    <Typography
-          fontWeight="bold"
-          fontSize="32px"
-          color="primary"
-          onClick={() => navigate("/home")}
-         
-        >
-          Sociopedia
+const LoginPage = () => {
+  const theme = useTheme();
+  const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
+  return (
+    <Box>
+      <Box
+        width="100%"
+        backgroundColor={theme.palette.background.alt}
+        p="1rem 6%"
+        textAlign="center"
+      >
+        <Typography fontWeight="bold" fontSize="32px" color="primary">
+          New Social Media
         </Typography>
-        </Box>
-{/*form boxes here*/} 
-        <Box
-        width = {isNonMobileScreens ? "50%" : "93%"}
-        p = "2rem"
-        m = "2rem auto"
-        borderRadius = "1.5rem"
-        backgroundColor = {theme.pallete.background.alt}
-       >
-        <Typography fontWeight = "500" variant = "h5" sx = {{mb: "1.5rem"}}>
-            Welcome to Scoiapedia!
+      </Box>
+
+      <Box
+        width={isNonMobileScreens ? "50%" : "93%"}
+        p="2rem"
+        m="2rem auto"
+        borderRadius="1.5rem"
+        backgroundColor={theme.palette.background.alt}
+      >
+        <Typography fontWeight="500" variant="h5" sx={{ mb: "1.5rem" }}>
+          Welcome to a new type of social media
         </Typography>
         <Form />
-         </Box>
+      </Box>
     </Box>
-    };
-    export default loginPage;
+  );
+};
+
+export default LoginPage;
